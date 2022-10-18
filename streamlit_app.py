86daaf905c139974920fe04d39c1c99fa84f32d2
Tamizharasan <116019427+Tamizh86@users.noreply.github.com>
2022-10-18 14:41:49 +0530

    Update streamlit_app.py
    Added Dinner Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
-streamlit.title('My first Streamlit Example...')
+streamlit.title('New Healthy Diner')
 
-streamlit.header('Hello World!')
-streamlit.text('Text1')
-streamlit.text('Text2')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocoda Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
