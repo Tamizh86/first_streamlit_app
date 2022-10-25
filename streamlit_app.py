2a2a6217f68c1c63f63a45c59a715a8636200571
Tamizharasan <116019427+Tamizh86@users.noreply.github.com>
2022-10-25 11:31:24 +0530

    Update streamlit_app.py
    cooooo

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ width=400,
 caption= product_caption
 )
 
- streamlit.write('Price: ', df2[1])
+streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
 
