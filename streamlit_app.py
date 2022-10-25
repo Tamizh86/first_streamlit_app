cc105be26f4031dcd0035629018ee9b5ed1e5c72
Tamizharasan <116019427+Tamizh86@users.noreply.github.com>
2022-10-25 11:21:00 +0530

    Update streamlit_app.py
    New code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,36 @@
 import streamlit
-import pandas as pd
 import snowflake.connector
+import pandas
+
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-streamlit.title('New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocoda Toast')
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+
+df = pandas.DataFrame(my_catalog)
+
+color_list = df[0].values.tolist()
+
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
+color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+
+streamlit.image(
+df2[0],
+width=400,
+caption= product_caption
+)
+
+ streamlit.write('Price: ', df2[1])
+streamlit.write('Sizes Available: ',df2[2])
+streamlit.write(df2[3])
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.dataframe(my_fruit_list)
+               
